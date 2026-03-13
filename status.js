dddc5257e70e2a0ee16480d3bd4d231ace2e4e00
MEMDMP <wjr0875.br@gmail.com>
2026-03-14 04:17:30 +0900

    Update status.js

--- status.js
+++ status.js
@@ -21,7 +21,7 @@ const commands = {
     profile: "프로필로 이동 중...",
 friends: function() {
     setTimeout(() => {
-        outputDiv.innerText += "\n\n1. 엑시트 \n2. XYZ \n3. 이버스 \n4. 디프다";
+        outputDiv.innerText += "\n\n1. 엑시트 \n2. XYZ \n3. 이버스 \n4. 디프다 \n5. 롭-시 (내 베프 ? !)";
     }, 1500); 
     return "친구 목록 불러오는 중..."; 
 },
